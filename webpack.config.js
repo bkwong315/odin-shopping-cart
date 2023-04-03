cb24a355a9e45ec2209b72f165c6a01315565d99
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-04-02 22:49:39 -0700

    Setup initial working environment

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { argv } = require('process');
+
+module.exports = (argv) => {
+  let config = {
+    entry: './src/index.tsx',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'dist'),
+      },
+      port: 8000,
+      open: true,
+      hot: true,
+      compress: true,
+      historyApiFallback: true,
+    },
+    devtool: argv.mode === 'development' ? 'inline-source-map' : 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.(ts|tsx)x$/,
+          exclude: /node_modules/,
+          loader: 'ts-loader',
+        },
+        {
+          test: /\.css$/i,
+          include: path.resolve(__dirname, 'src'),
+          use: ['style-loader', 'css-loader', 'postcss-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        fileName: 'index.html',
+        title: 'Shop Name',
+        templateContent: ({ htmlWebpackPlugin }) => `<html>
+            <head>
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <meta charset="utf-8" />
+              <title>${htmlWebpackPlugin.options.title}</title>
+            </head>
+            <body>
+              <div class='root'></div>
+            </body>
+          </html>`,
+      }),
+    ],
+  };
+
+  return config;
+};
