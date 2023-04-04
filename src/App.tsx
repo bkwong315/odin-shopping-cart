e59af5a510d7aee6bf1c1633a06a80a5d2714a2b
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-04-03 19:18:46 -0700

    Add ProductPage and cart state

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Shop from './pages/Shop';
+import Product from './interfaces/Product';
 import Header from './components/Header/Header';
 import ProductPage from './pages/ProductPage';
+import ShoppingCart from './interfaces/ShoppingCart';
+
+const ProductList: { [productId: string]: Product } = {
+  product_id_1: {
+    id: 'product_id_1',
+    name: 'product_1',
+    price: 5.0,
+  },
+  product_id_2: {
+    id: 'product_id_2',
+    name: 'product_2',
+    price: 7.5,
+  },
+  product_id_3: {
+    id: 'product_id_3',
+    name: 'product_3',
+    price: 10.0,
+  },
+  product_id_4: {
+    id: 'product_id_4',
+    name: 'product_4',
+    price: 20.0,
+  },
+};
 
 const App = () => {
+  const [cart, setCart] = useState<ShoppingCart>({});
+
+  const addToCart = (product: Product) => {
+    setCart(() => {
+      const newCart = { ...cart };
+      newCart[product.id] = {
+        product,
+        quantity:
+          newCart[product.id] !== undefined
+            ? newCart[product.id].quantity + 1
+            : 0,
+      };
+
+      return newCart;
+    });
+  };
+
   return (
     <Router>
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='shop' element={<Shop />}></Route>
-        <Route path='/shop/product/:productId' element={<ProductPage />} />
+        <Route
+          path='/shop/product/:productId'
+          element={
+            <ProductPage ProductList={ProductList} addToCart={addToCart} />
+          }
+        />
       </Routes>
     </Router>
   );
--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-const ProductPage = () => {
+import Product from '../interfaces/Product';
+
+interface ProductPageProps {
+  addToCart: (product: Product) => void;
+  ProductList: { [productId: string]: Product };
+}
+
+const ProductPage = (props: ProductPageProps) => {
   const params = useParams();
+  const { addToCart, ProductList } = props;
 
   return (
     <div className='text-center'>
       <h3>{params.productId} Page</h3>
-      <button className='bg-purple-800 rounded px-4 py-1 text-white font-bold hover:scale-105 transition-all'>
+      <button
+        onClick={addToCart.bind(null, ProductList[params.productId])}
+        className='bg-purple-800 rounded px-4 py-1 text-white font-bold hover:scale-105 transition-all'>
         Add to Cart
       </button>
     </div>
