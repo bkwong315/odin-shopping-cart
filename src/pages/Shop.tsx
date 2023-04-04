eebca5db461017e6f438b40eb33b03fed37404fa
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-04-03 20:19:38 -0700

    Add links to product pages

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Shop = () => <div className='text-center'>Shop</div>;
+import Product from '../interfaces/Product';
+
+interface ShopProps {
+  ProductList: { [productId: string]: Product };
+}
+
+const Shop = (props: ShopProps) => {
+  const { ProductList } = props;
+  return (
+    <div className='text-center'>
+      <h3>Shop</h3>{' '}
+      {Object.entries(ProductList).map((product, idx) => {
+        const { id, name, price } = product[1];
+
+        return (
+          <div key={idx}>
+            <Link to={`/shop/product/${id}`}>
+              {id} {name} {price}
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Shop;
