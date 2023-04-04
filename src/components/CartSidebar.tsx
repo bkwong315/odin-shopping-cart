5d8a700417e12a61bd705039d50acaba29db6ebe
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-04-04 16:53:17 -0700

    Add basic styling to CartSidebar

--- src/components/CartSidebar.tsx
+++ src/components/CartSidebar.tsx
@@ -10,23 +10,25 @@ const CartSidebar = (props: CartSidebarProps) => {
   const { items } = props;
 
   return (
-    <div>
-      <h3>Your Cart</h3>
-      {Object.entries(items).map((item, idx) => {
-        const { product, quantity } = item[1];
-
-        return (
-          <div key={idx}>
-            {product.name} {quantity}
-          </div>
-        );
-      })}
-      <div>
-        Subtotal:{' '}
-        {Object.entries(items).reduce(
-          (reducer, item) => reducer + item[1].product.price * item[1].quantity,
-          0
-        )}
+    <div className='fixed w-screen h-screen top-0 left-0 grid place-items-end bg-slate-800 bg-opacity-30'>
+      <div className='bg-purple-600 right-0 w-[20%] h-full p-4 text-white font-bold'>
+        <h3>Your Cart</h3>
+        {Object.entries(items).map((item, idx) => {
+          const { product, quantity } = item[1];
+          return (
+            <div key={idx}>
+              {product.name} {quantity}
+            </div>
+          );
+        })}
+        <div>
+          Subtotal:{' '}
+          {Object.entries(items).reduce(
+            (reducer, item) =>
+              reducer + item[1].product.price * item[1].quantity,
+            0
+          )}
+        </div>
       </div>
     </div>
   );
