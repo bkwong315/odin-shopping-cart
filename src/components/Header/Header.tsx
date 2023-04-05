e6ad85a4aecaaabf3823d9c4c792bc16613d331a
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-04-04 19:15:26 -0700

    Update header css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className='flex justify-between p-4 bg-purple-700 text-white font-bold'>
+    <header className='flex justify-between px-8 py-4 bg-purple-700 text-white font-bold'>
       <h2>Shop Name</h2>
-      <Link
-        to='/shop'
-        className='hover:text-sky-300 hover:underline underline-offset-2'>
-        Shop
-      </Link>
-      <Link
-        to='/cart'
-        className='hover:text-sky-300 hover:underline underline-offset-2'>
-        My Cart
-      </Link>
+      <div className='flex gap-4'>
+        <Link
+          to='/shop'
+          className='hover:text-sky-300 hover:underline underline-offset-2'>
+          Shop
+        </Link>
+        <Link
+          to='/cart'
+          className='hover:text-sky-300 hover:underline underline-offset-2'>
+          My Cart
+        </Link>
+      </div>
     </header>
   );
 };
