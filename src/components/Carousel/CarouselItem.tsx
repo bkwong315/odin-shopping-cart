e7fad1a832bfbfffe7dc91b8568839304efc9a1c
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-04-07 16:48:15 -0700

    Change font family of header to klavika-medium

--- src/components/Carousel/CarouselItem.tsx
+++ src/components/Carousel/CarouselItem.tsx
@@ -17,7 +17,7 @@ const CarouselItem = (props: CarouselItemProps) => {
     <div className='flex gap-4 text-white'>
       <img src={imgUrl} alt={altText} />
       <div className='flex flex-col justify-center gap-4 w-[45%] mb-8'>
-        <h3 className='font-bold text-4xl'>{headerText}</h3>
+        <h3 className='font-klavika-medium text-4xl '>{headerText}</h3>
         <p className='text-sm'>{description}</p>
         <button className='px-10 py-2 border-solid border-white border w-max font-bold uppercase hover:bg-white/[.15]'>
           {btnText}
