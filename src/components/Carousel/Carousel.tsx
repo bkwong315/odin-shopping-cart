e59726f5a74fb320b867b10a4014a08e446d06db
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-04-07 16:27:09 -0700

    Add CarouselItem props and data

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -3,9 +3,54 @@ import React from 'react';
 import CarouselItem from './CarouselItem';
 
 const Carousel = () => {
+  const carouselItemDetails = [
+    {
+      imgUrl: './assets/imgs/carousel/ryzen-processors.jpg',
+      altText: 'ryzen processors',
+      headerText: `The Ultimate Processor for Gaming`,
+      description: `Take on the most demanding games with the massive performance boost delivered by AMD Ryzen 7000X3D Series processors with AMD 3D V-Cache technology.`,
+      btnText: 'Shop Now',
+    },
+    {
+      imgUrl: './assets/imgs/carousel/amd-bundles.jpg',
+      altText: 'amd bundles',
+      headerText: `Bundle and Save with AMD Ryzen™ Processors and AMD Radeon™ Graphics Deals!`,
+      description: `Get up to $100 in additional savings when you purchase select AMD Ryzen™ Processors and AMD Radeon™ Graphics together!`,
+      btnText: 'Learn More',
+    },
+    {
+      imgUrl: './assets/imgs/carousel/last-of-us-bundle.jpg',
+      altText: 'last of us bundle',
+      headerText: `AMD Radeon™ THE LAST OF US™ Game Bundle`,
+      description: `Get THE LAST OF US™ PART I when you buy select AMD Radeon™ RX 7000 and RX 6000 Series graphics cards`,
+      btnText: 'Learn More',
+    },
+    {
+      imgUrl: './assets/imgs/carousel/jedi-survivor-bundle.jpg',
+      altText: 'jedi survivor bundle',
+      headerText: `AMD Ryzen™ 7000 Series Game Bundle`,
+      description: `Get STAR WARS Jedi: Survivor™ when you buy select AMD Ryzen™ 7000 Series Processors.`,
+      btnText: 'Learn More',
+    },
+  ];
+
   return (
-    <div className='bg-black'>
-      <CarouselItem />
+    <div className='relative bg-black'>
+      <button className='absolute left-[2%] top-1/2 -translate-y-1/2'>
+        <img
+          src='./assets/icons/chevron-left-solid.svg'
+          alt='left-chevron'
+          className='h-8 hover:brightness-[.25]'
+        />
+      </button>
+      <button className='absolute right-[2%] top-1/2 -translate-y-1/2'>
+        <img
+          src='./assets/icons/chevron-right-solid.svg'
+          alt='right-chevron'
+          className='h-8 hover:brightness-[.25]'
+        />
+      </button>
+      <CarouselItem data={carouselItemDetails[3]} />
     </div>
   );
 };
--- src/components/Carousel/CarouselItem.tsx
+++ src/components/Carousel/CarouselItem.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 
-const CarouselItem = () => {
+interface CarouselItemProps {
+  data: {
+    imgUrl: string;
+    altText: string;
+    headerText: string;
+    description: string;
+    btnText: string;
+  };
+}
+
+const CarouselItem = (props: CarouselItemProps) => {
+  const { imgUrl, altText, headerText, description, btnText } = props.data;
+
   return (
     <div className='flex gap-4 text-white'>
-      <img src='./assets/imgs/carousel/amd-bundles.jpg' alt='amd bundles' />
+      <img src={imgUrl} alt={altText} />
       <div className='flex flex-col justify-center gap-4 w-[45%] mb-8'>
-        <h3 className='font-bold text-4xl'>
-          Bundle and Save with AMD Ryzen&trade; Processors and AMD Radeon&trade;
-          Graphics Deals!
-        </h3>
-        <p className='text-sm'>
-          Get up to $100 in additional savings when you purchase select AMD
-          RyzenTM Processors and AMD Radeon&trade; Graphics Together!
-        </p>
+        <h3 className='font-bold text-4xl'>{headerText}</h3>
+        <p className='text-sm'>{description}</p>
         <button className='px-10 py-2 border-solid border-white border w-max font-bold uppercase hover:bg-white/[.15]'>
-          Learn More
+          {btnText}
         </button>
       </div>
     </div>
