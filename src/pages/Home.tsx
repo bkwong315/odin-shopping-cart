459372e78fd8cf2a164d37a0c858b6b9b005a38a
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-04-07 23:22:23 -0700

    Add ShopDisplayLink and fix overflow issues

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
 import Carousel from '../components/Carousel/Carousel';
+import ShopDisplayLink from '../components/ShopDisplayLink/ShopDisplayLink';
 
 const Home = () => {
   return (
-    <div>
+    <div className='grid justify-items-center gap-12 w-full max-w-full overflow-auto'>
       <Carousel />
+      <div className='flex justify-center gap-8 w-full max-w-[1440px]'>
+        <ShopDisplayLink
+          imgUrl='./assets/imgs/processor.jpg'
+          headerText={'processors'}
+          linkUrl='/processors'
+        />
+        <ShopDisplayLink
+          imgUrl='./assets/imgs/graphics-card.jpg'
+          headerText={'graphics cards'}
+          linkUrl='/graphics-cards'
+        />
+      </div>
     </div>
   );
 };
