65ad79052b32bf9cb0c2d5f96f52212d33094434
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-04-08 18:26:05 -0700

    Add header to FeaturedProducts

--- src/components/FeaturedProducts/FeaturedProducts.tsx
+++ src/components/FeaturedProducts/FeaturedProducts.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const FeaturedProducts = () => {
-  return <div>Featured Products</div>;
+  return (
+    <div className='flex justify-center gap-8 w-full max-w-[1440px]'>
+      <header className='flex justify-between items-center w-full'>
+        <h3 className='uppercase tracking-wide font-klavika-medium text-4xl'>
+          Featured Products
+        </h3>
+        <Link
+          className='uppercase text-sm underline text-gray-600 hover:text-gray-900 transition-[color] duration-500'
+          to={'/shop'}>
+          View All Products
+        </Link>
+      </header>
+    </div>
+  );
 };
 
 export default FeaturedProducts;
