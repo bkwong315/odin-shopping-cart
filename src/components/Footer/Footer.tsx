97f7574e0d11c5e66b65ad20c48e805dac50bb67
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-04-10 19:37:21 -0700

    Link SocialLinksList to Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SocialLinkList from './SocialLinkList';
 
 const Footer = () => {
   return (
     <footer className='bg-black text-white py-8'>
-      <div className='flex justify-between max-w-[1440px] m-auto'>
+      <div className='flex justify-between items-center max-w-[1440px] m-auto'>
         <div className='flex items-center gap-4'>
           <Link to='/'>
             <img
@@ -19,6 +20,7 @@ const Footer = () => {
             Store
           </Link>
         </div>
+        <SocialLinkList />
       </div>
     </footer>
   );
