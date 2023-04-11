cb6d7cc85cb3591eb6e5cdd4ad6f0f58d8b3e159
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-04-10 19:36:59 -0700

    Add SocialLinkItem animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,38 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fade-grow': 'fade-grow 300ms linear forwards',
+        'fade-out': 'fade-out 300ms linear forwards',
+      },
+      keyframes: {
+        'fade-grow': {
+          '0%': {
+            height: '75%',
+          },
+          '50%': {
+            height: '100%',
+          },
+          '100%': {
+            height: '100%',
+          },
+        },
+        'fade-out': {
+          '0%': {
+            opacity: '100%',
+            height: '100%',
+          },
+          '50%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '0',
+            height: '75%',
+          },
+        },
+      },
+    },
     fontFamily: {
       'klavika-medium': ['klavika-medium', 'arial', 'sans-serif'],
     },
