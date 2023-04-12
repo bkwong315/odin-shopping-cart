55f8b5e017a0f47021b9282262d5af128ef000ce
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-04-11 17:13:20 -0700

    Add header for shop page content (WIP)

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -10,20 +10,30 @@ interface ShopProps {
 const Shop = (props: ShopProps) => {
   const { ProductList } = props;
   return (
-    <div className='text-center'>
-      <h3>Shop</h3>{' '}
-      {Object.entries(ProductList).map((product, idx) => {
-        const { id, name, price } = product[1];
-
-        return (
-          <div key={idx}>
-            <Link to={`/shop/product/${id}`}>
-              {id} {name} {price}
-            </Link>
+    <main className='max-w-[1440px] m-auto p-4'>
+      <header>
+        <div className='grid gap-3'>
+          <div className='text-sm'>
+            Home &gt; <span className='text-neutral-500'>Processors</span>
           </div>
-        );
-      })}
-    </div>
+          <h3 className='uppercase text-4xl font-klavika-medium'>Processors</h3>
+        </div>
+      </header>
+      <div className='text-center'>
+        <h3>Shop</h3>{' '}
+        {Object.entries(ProductList).map((product, idx) => {
+          const { id, name, price } = product[1];
+
+          return (
+            <div key={idx}>
+              <Link to={`/shop/product/${id}`}>
+                {id} {name} {price}
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </main>
   );
 };
 
