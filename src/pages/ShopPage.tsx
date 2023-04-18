7437a50ec0fc61832a981ed1b1b9526081c601cc
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-04-18 15:37:59 -0700

    Fix display text when total length < 9

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -20,6 +20,7 @@ const Shop = (props: ShopProps) => {
   const { productList } = props;
   const [sortMethod, setSortMethod] = useState('featured');
   const [startIdx, setStartIdx] = useState(0);
+  const productListLength = Object.entries(productList).length;
 
   useEffect(() => {
     setStartIdx(0);
@@ -207,11 +208,15 @@ const Shop = (props: ShopProps) => {
         </div>
         <div className='flex justify-between items-center w-full mt-12'>
           <div>
-            Displaying {startIdx + 1} -{' '}
-            {startIdx + 9 < Object.entries(productList).length
-              ? startIdx + 9
-              : Object.entries(productList).length}{' '}
-            of {Object.entries(productList).length}
+            {productListLength > 9
+              ? `Displaying ${startIdx + 1} -
+            ${
+              startIdx + 9 < productListLength
+                ? startIdx + 9
+                : productListLength
+            }
+            of ${productListLength}`
+              : `Displaying ${productListLength} of ${productListLength}`}
           </div>
           <div className='flex items-center gap-2'>
             <span className='font-noto-sans-medium font-medium'>Sort by:</span>
