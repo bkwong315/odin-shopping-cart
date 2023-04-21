e30ced74f9540f923e66b3e23c6232affea95e04
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-04-21 16:53:16 -0700

    Update interface to store ProductList query info

--- src/interfaces/ShoppingCart.ts
+++ src/interfaces/ShoppingCart.ts
@@ -1,5 +1,15 @@
+import { ProductListType } from '../ProductList';
+import Processor from './Processor';
+import GraphicsCard from './GraphicsCard';
+
 interface ShoppingCart {
-  [id: string]: { productInfo: [string, string]; quantity: number };
+  [id: string]: {
+    productInfo: [
+      keyof (ProductListType['processors'] | ProductListType['graphics_cards']),
+      Processor['product_type'] | GraphicsCard['product_type']
+    ];
+    quantity: number;
+  };
 }
 
 export default ShoppingCart;
