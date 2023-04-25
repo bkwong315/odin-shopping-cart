fa614f4fed85c3a8730330fc1511dd49c28b4939
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-04-24 20:54:14 -0700

    Add map product info to CartItem

--- src/components/CartDisplay/CartDisplay.tsx
+++ src/components/CartDisplay/CartDisplay.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
-const CartDisplay = () => {
-  return <div></div>;
+import ShoppingCart from '../../interfaces/ShoppingCart';
+import ProductList, { ProductListType } from '../../ProductList';
+
+import CartItem from './CartItem';
+
+interface CartDisplayProps {
+  cart: ShoppingCart;
+}
+
+const CartDisplay = (props: CartDisplayProps) => {
+  const { cart } = props;
+
+  const cartItems = Object.values(cart).map((item, idx) => {
+    const product =
+      ProductList[
+        `${item.productInfo[1].replace('-', '_')}` as keyof ProductListType
+      ][item.productInfo[0]];
+
+    return <CartItem product={product} quantity={item.quantity} key={idx} />;
+  });
+
+  return <div>{cartItems}</div>;
 };
 
 export default CartDisplay;
