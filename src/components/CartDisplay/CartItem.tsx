b11f39be007c27c0c48bb6848f821cba87d4a674
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-04-25 20:43:10 -0700

    Add content and styling to CartItem

--- src/components/CartDisplay/CartItem.tsx
+++ src/components/CartDisplay/CartItem.tsx
@@ -9,10 +9,56 @@ interface CartItemProps {
 
 const CartItem = (props: CartItemProps) => {
   const { product, quantity } = props;
+
   return (
-    <div>
-      {product.name} {quantity}
-    </div>
+    <tr className='border-b text-sm'>
+      <td className='grid place-items-center py-8'>
+        <img
+          src={Object.values(product.imgs)[1]}
+          alt={`${product.name}`}
+          className='w-12 h-12'
+        />
+      </td>
+      <td className='font-noto-san-medium font-bold'>{product.name}</td>
+      <td className='text-center text-neutral-500'>
+        $
+        {product.salePrice
+          ? parseFloat(product.salePrice).toFixed(2)
+          : parseFloat(product.price).toFixed(2)}
+      </td>
+      <td className='text-center'>
+        <input
+          type='number'
+          value={quantity}
+          className='text-center border-neutral-300 border border-x-0 focus-visible:border-x-2 appearance-none w-10'
+        />
+      </td>
+      <td className='text-center text-neutral-500'>
+        $
+        {`${
+          product.salePrice
+            ? (parseFloat(product.salePrice) * quantity).toFixed(2)
+            : (parseFloat(product.price) * quantity).toFixed(2)
+        }`}
+      </td>
+      <td className='text-center pr-4'>
+        <button>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 100 100'
+            className='w-6 h-6'>
+            <path
+              stroke='#000'
+              strokeWidth='12'
+              d='M 20, 20
+                    L 80, 80
+                    M 20, 80
+                    L 80, 20'
+            />
+          </svg>
+        </button>
+      </td>
+    </tr>
   );
 };
 
