57402520ed2ab2799ea01a84a892bd5c957a7703
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-05-01 18:20:37 -0700

    Add func to check type and export BundleType

--- src/Bundles.ts
+++ src/Bundles.ts
@@ -1,25 +1,36 @@
+type BundleType = {
+  id: string;
+  product_type: string;
+  name: string;
+  price: string;
+  img: string;
+};
+
 type BundlesType = {
-  [bundleId: string]: {
-    id: string;
-    name: string;
-    price: string;
-    img: string;
-  };
+  [bundleId: string]: BundleType;
 };
 
 const Bundles: BundlesType = {
   ryzen_7000_star_wars: {
     id: 'ryzen_7000_star_wars',
+    product_type: 'bundle',
     name: 'Ryzen 7000 Series STAR WARS Jedi: Survivor Game Bundle',
     price: '0',
-    img: './assets/imgs/carousel/jedi-survivor-bundle',
+    img: './assets/imgs/carousel/jedi-survivor-bundle.jpg',
   },
   radeon_graphics_tlou: {
     id: 'radeon_graphics_tlou',
+    product_type: 'bundle',
     name: 'AMD Radeon™ RX Graphics Cards Game Bundle',
     price: '0',
-    img: './assets/imgs/carousel/last-of-us-bundle',
+    img: './assets/imgs/carousel/last-of-us-bundle.jpg',
   },
 };
 
+function isBundle(object: any): object is BundleType {
+  return object.product_type === 'bundle';
+}
+
 export default Bundles;
+
+export { BundlesType, BundleType, isBundle };
